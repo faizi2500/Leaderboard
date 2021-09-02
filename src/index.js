41403dd921d06f8443d702747a40ad3244e854dd
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-09-02 18:09:27 +0500

    Add temporaray game link to allow users to practice

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import showContent from './showContent.js';
 import getInput from './getInput.js';
 import sendScores from './sendScores.js';
 import getFromAPI from './getFromAPI.js';
+import createGame from './createGame.js';
 
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-const urlWithGame = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/2aj9ZLFs3uhbhcYLybFr/scores/'
+let newUrlWithGame;
+let urlWithGame = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/K6M7cT3zpdPIl0p6GBmZ/scores/';
 const submit = document.getElementById('submit-btn');
 const refresh = document.getElementById('refresh-btn');
+const newGame = document.getElementById('new-link');
 let list = [];
 
 submit.addEventListener('click', async (e) => {
@@ -15,6 +18,7 @@ submit.addEventListener('click', async (e) => {
   const player = await getInput();
   const name = player.playerName;
   const score = player.playerScore;
+  // const newGameData = score + name;
   await sendScores(name, score, urlWithGame);
 });
 
@@ -22,13 +26,30 @@ refresh.addEventListener('click', async (e) => {
   e.preventDefault();
   const getData = await getFromAPI(urlWithGame);
   const totalPlayers = getData.result;
+  // const newGame = getData.result;
   list = totalPlayers;
   showContent(list);
 });
 
+newGame.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const result = await createGame(url);
+  let gameID = result.result;
+  gameID = gameID.substring(14, 34);
+  newUrlWithGame = `${url}${gameID}/scores/`;
+  list = [];
+  console.log(newUrlWithGame);
+  urlWithGame = newUrlWithGame
+  console.log(urlWithGame)
+})
+
 window.onload = async () => {
+  console.log(newUrlWithGame);
+  console.log(urlWithGame);
   const getData = await getFromAPI(urlWithGame);
   const totalPlayers = getData.result;
   list = totalPlayers;
   showContent(list);
 };
+
+
