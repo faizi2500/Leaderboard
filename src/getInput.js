98d14de9fcac201e07fd36c73463623f7379fc03
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-09-02 15:13:05 +0500

    Add setTimeout function to remove successfully shown message after 3 seconds

--- src/getInput.js
+++ src/getInput.js
@@ -5,12 +5,18 @@ const userScore = document.getElementById('user-score');
 
 const getInput = async () => {
   const showError = document.querySelector('.error-style');
+  const added = document.querySelector('.added');
   if (userName.value === '' || userScore.value === '') {
     showError.style.display = 'block';
   } else {
     const playerName = userName.value;
     const playerScore = userScore.value;
     showError.style.display = 'none';
+    added.style.display = 'block'
+    setTimeout(() => {
+      added.style.display = 'none'
+    }, 3000)
+    // added.style.display = 'block'
     userName.value = '';
     userScore.value = '';
     return { playerName, playerScore };
